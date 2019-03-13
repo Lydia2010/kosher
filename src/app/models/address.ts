bf76cb2896a337b0936587c86715b8110cdd375a
Lydia2010 <lidiya.s@gmail.com>
2019-03-13 14:22:55 -0400

    added basic model

---
+++ src/app/models/address.ts
@@ -0,0 +1,12 @@
+export class Address {
+  // region Attributes
+  streetNumber: number;
+  streetName: string;
+  appartment: string;
+  city: string;
+  province: string;
+  postalCode: string;
+  country: string;
+  // endregion
+
+}
---
+++ src/app/models/review.ts
@@ -0,0 +1,10 @@
+export class Review {
+  // region Attribute
+  author: number;
+  rating: number;
+  tirle: string;
+  body: string;
+  date: Date;
+  // endregion
+
+}
