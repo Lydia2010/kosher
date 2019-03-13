10d9b01b66a2584b229dda8e786e2d76d5e14859
Lydia2010 <lidiya.s@gmail.com>
2019-03-13 16:32:44 -0400

    version3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
 import {Component, OnInit} from '@angular/core';
+import {Restaurant} from './models/restaurant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'kosher';
-  cardTitle = 'This is a card';
-  cardSubtitle = 'This is subtitlecard';
-  cardContetnt = 'This is a very nice first card';
-  // Init method
-  ngOnInit(): void {
-    this.title = 'WORD!!!!';
+export class AppComponent  {
+  rests: Restaurant[];
+
+  constructor() {
+    this.rests = [];
+    let temp = new Restaurant();
+    temp.name = 'Tav Cafe';
+    temp.phone = '(514)-112-1112';
+    temp.rating = 3;
+    temp.priceRange = 3;
+    temp.website = 'http://www.google.ca';
+    this.rests.push(temp);
+
+    temp = new Restaurant();
+    temp.name = 'Piza Piza';
+    temp.phone = '(514)-333-3333';
+    temp.rating = 4;
+    temp.priceRange = 4;
+    this.rests.push(temp);
+
+    temp = new Restaurant();
+    temp.name = 'Pasta';
+    temp.rating = 2;
+    temp.priceRange = 2;
+    temp.website = 'http://www.google.ca';
+    this.rests.push(temp);
   }
-
-
 }
 
--- src/app/view/rest-card/rest-card.component.ts
+++ src/app/view/rest-card/rest-card.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Restaurant} from '../../models/restaurant';
 
 @Component({
   selector: 'app-rest-card',
   templateUrl: './rest-card.component.html',
   styleUrls: ['./rest-card.component.scss']
 })
-export class RestCardComponent implements OnInit {
+export class RestCardComponent implements OnInit  {
+  @Input() restaurant: Restaurant;
+  // now = new Date();
+  constructor() {
+    // this.restaurant = new Restaurant();
 
-  constructor() { }
-
-  ngOnInit() {
   }
+    ngOnInit(): void {
+    // this.restaurant.name = 'Tav Cafe';
+    // this.restaurant.phone = '(514)-112-1112';
+    // this.restaurant.rating = 3;
+    // this.restaurant.priceRange = 3;
+    // this.restaurant.website = 'http://www.google.ca';
+  }
+
+
 
 }
